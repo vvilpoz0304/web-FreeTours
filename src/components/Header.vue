eb456f7a337497964c2882e7da7b606c67bfec4e
vvilpoz0304 <vvilpoz0304@g.educaand.es>
2025-02-06 14:52:48 +0100

    Minor Changes, tests

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+    <header>Esto es la cabecera</header>
+</template>
+
+<style scoped>
+    header{
+        background-color: lightcoral;
+    }
+</style>
