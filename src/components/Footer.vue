08040440c224e6c970d5edb73a8c42bb506787b0
vvilpoz0304 <vvilpoz0304@g.educaand.es>
2025-02-09 16:05:46 +0100

    Pestañas de administrador añadidas y logos de footer añadidos

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-    <footer class="footer">
-      <div>
+  <footer class="footer">
+    <div>
       <p>Información de Contacto:</p>
       <ul>
         <li>Correo</li>
@@ -8,28 +8,42 @@
         <li>Ubicacion</li>
       </ul>
     </div>
-    <p xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/"><span property="dct:title">Web Freetours 2ºDaw</span> by <a rel="cc:attributionURL dct:creator" property="cc:attributionName" href="https://github.com/valentinVilla05">Valentin Villa</a> is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY-SA 4.0</a></p>
-      <div>
-        <p> <a href="#">Terminos y Condiciones </a></p>
-        <p> <a href="#">Preguntas Frecuentes </a></p>
-      </div>
-    </footer>
-  </template>
-  
-  <style scoped>
-  .footer {
-    background: rgb(125, 179, 125);
-    color: white;
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    display:  flex;
-    align-items: center;
-    justify-content: space-between;
-    text-align: center;
-  }
-  .footer div{
-    margin: 1em 2em 0em 2em;
-  }
-  </style>
-  
+    <div>
+      <p id="iconos">
+        <a href=""><img src="../assets/images/instagram.png" alt="Instagram"></a>
+        <a href=""><img src="../assets/images/twitter.png" alt="X"></a>
+        <a href=""><img src="../assets/images/tiktok.png" alt="TikTok"></a>
+      </p>
+      <p xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/"><span property="dct:title">Web
+          Freetours 2ºDaw</span> by <a rel="cc:attributionURL dct:creator" property="cc:attributionName"
+          href="https://github.com/valentinVilla05">Valentin Villa</a> is licensed under <a
+          href="https://creativecommons.org/licenses/by-sa/4.0/?ref=chooser-v1" target="_blank"
+          rel="license noopener noreferrer" style="display:inline-block;">CC BY-SA 4.0</a></p>
+    </div>
+    <div>
+      <p> <a href="#">Terminos y Condiciones </a></p>
+      <p> <a href="#">Preguntas Frecuentes </a></p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  background: rgb(125, 179, 125);
+  color: white;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  text-align: center;
+}
+
+.footer div {
+  margin: 1em 2em 0em 2em;
+}
+#iconos a{
+  margin: 0em 1em 0em 1em;
+}
+</style>
--- src/views/UserManagement.vue
+++ src/views/UserManagement.vue
@@ -1,26 +1,86 @@
 <script setup>
-    // Declaramos la URL para la API para mostrar todos los usuarios
-    const API = "http://localhost/freetours/api.php"
-    let users = [];
-    // Conseguimos un array con los usuarios mediante un fecth;
+// Importamos todo lo necesario;
+import { ref } from 'vue';
 
-    function getData(){
-        fetch(API + "/usuarios").then((response) => {
-            if(!response.ok){
-                console.log("No tengo ni idea, definitivamente");
-            } return response.json();
-        }).then((data) => console.log(data));
+// Declaramos la URL para la API para mostrar todos los usuarios
+const API = "http://localhost/freetours/api.php"
+let users = ref([]);
+// Conseguimos un array con los usuarios mediante un fecth;
+
+async function getData() {
+    try {
+        const response = await fetch(API + "/usuarios");
+
+        if (!response.ok) {
+            throw new Error("No tengo ni idea de lo que estoy haciendo;");
+        }
+
+        users.value = await response.json(); // Guardamos los datos en la variable global
+        console.log("Usuarios obtenidos correctamente:", users);
+    } catch (error) {
+        alert(`Error al obtener datos: ${error.message}`);
     }
-    getData();
+}
+
+getData().then(() => {
+    users.value = users.value.map(e => ({ Nombre: e.nombre, Email: e.email, Rol: e.rol }));
+    console.log(users);
+})
+
 </script>
 <template>
-    <h1>Wuaaa</h1>
-    <ul>
-        <li v-for="">
-
-        </li>
-    </ul>
+    <!-- Añadimos las pestañas mediante bootstrap.
+    Mostraremos unas u otras dependiendo del rol del usuario registrado
+    esto  lo haremos mediante el condional "v-if" -->
+    <div>
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <li class="nav-item" role="presentation">
+                <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane"
+                    type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Gestion de Usuarios</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane"
+                    type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Creacion de Rutas</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact-tab-pane"
+                    type="button" role="tab" aria-controls="contact-tab-pane" aria-selected="false">Cancelacion de Rutas</button>
+            </li>
+        </ul>
+        <div class="tab-content" id="myTabContent">
+            <!--Contenido de la ventana de Gestion de Usuarios-->
+            <div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab"
+                tabindex="0">
+                <ul>
+                    <li v-for="user in users">Nombre: {{ user.Nombre }}</li>
+                </ul>
+            </div>
+            <!--Contenido de la ventana de la Creacion de Rutas-->
+            <div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">
+                Pestaña para la creacion de rutas
+            </div>
+            <!--Contenido de la ventana de la cancelacion de rutas-->
+            <div class="tab-pane fade" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">
+                Pestaña para la cancelacion de rutas
+            </div>
+            <div class="tab-pane fade" id="disabled-tab-pane" role="tabpanel" aria-labelledby="disabled-tab"
+                tabindex="0">...</div>
+        </div>
+    </div>
 </template>
-<style scoped>
 
+<style scoped>
+    button{
+        background-color: lightgreen;
+        color: black;
+    }
+    /*
+    "aria-selected" es la clase que se le asigna automaticamente a la tab activa
+     !!important se usa para sobrescribir los estilos de Bootstrap.
+    */
+    .nav-tabs .nav-link[aria-selected="true"] {
+    background-color: rgb(101, 172, 101) !important;
+    color: white !important; 
+    font-weight: bold;
+}
 </style>
