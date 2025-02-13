223f2a79ce3f7b31ef8c86f11191d3934f0764a3
vvilpoz0304 <vvilpoz0304@g.educaand.es>
2025-02-13 21:27:45 +0100

    Fondo del buscador añadido

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,21 @@
+<script setup>
+
+</script>
 <template>
-  Esta es la vista de home
-  Copy
-<div class="alert alert-success" role="alert">
-  <h4 class="alert-heading">Usuario Registrado correctamente!</h4>
-  <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
-  <hr>
-  <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
-</div>
+  <div class="fondo-buscador">
+    <div id="formBusqueda">
+      <input type="text">
+    </div>
+  </div>
+  <div class="listaRutas">
+    
+  </div>
 </template>
+
+<style scoped>
+.fondo-buscador{
+  background-image: url("/src/assets/images/fondoBuscador.avif");
+  background-size: cover;
+  height: 40vh;
+}
+</style>
